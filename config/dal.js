54d59852e45a3ccd870013b22d8d175232cac15c
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-09-19 18:52:40 -0400

    Delete unneeded comments and add a relevant one

--- config/dal.js
+++ config/dal.js
@@ -1,8 +1,4 @@
-//instructions:In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
-
-// selectAll()
-// insertOne()
-// updateOne()
+// Organize methods to apply SQL queries
 
 class DAL {
     // Import mySQL connection when class is instantiated
