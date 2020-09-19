08f842a08c376afca114f954d2e8ab09c9e81dc9
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-09-19 17:00:53 -0400

    Set db connection to work with JawsDB if available

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root1234",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root1234",
+        database: "burgers_db"
+    });
+};
 
 connection.connect((err) => {
     if (err) {
