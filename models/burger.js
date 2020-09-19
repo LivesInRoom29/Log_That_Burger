26c9b9d5d97be1267a17aa767560635357857d90
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-09-18 21:36:48 -0400

    remove console log and extra line; change variable name

--- models/burger.js
+++ models/burger.js
@@ -4,20 +4,18 @@ const dbManager = require('../config/burgersDbManager');
 // create the code that will call the ORM functions using burger specific input for the ORM.
 const burger = {
     all: function() {
-        console.log("get all burgers...")
         return dbManager.selectAll('burgers');
     },
     create: function(columns, values) {
         return dbManager.insertOne('burgers', columns, values);
     },
-    update: function(columnObjVal, condition) {
-        return dbManager.updateOne('burgers', columnObjVal, condition);
+    update: function(columnEqlVal, condition) {
+        return dbManager.updateOne('burgers', columnEqlVal, condition);
     },
     delete: function(condition) {
         return dbManager.deleteOne('burgers', condition);
     }
-}
-
+};
 
 //Export at the end of the burger.js file.
 
